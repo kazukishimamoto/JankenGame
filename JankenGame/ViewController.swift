bcb70a9375ad4836a5230bb03b4e5b8ca9591edf
KazukiShimamoto <jakky0529@gmail.com>
2018-08-02 13:46:13 +0900

    UIパーツの紐付け & リソース追加

--- JankenGame/ViewController.swift
+++ JankenGame/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var answerImageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
 }
 
